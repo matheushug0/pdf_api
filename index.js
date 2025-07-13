b41b20d54eac837e7c1cc59c068042f53734fc53
matheushug0 <mateus.hugo.mh@gmail.com>
2025-07-13 15:07:53 -0300

    testing

--- index.js
+++ index.js
@@ -36,10 +36,7 @@ app.post('/gerar-pdf', async (req, res) => {
 
     const page = await browser.newPage();
     await page.goto(url, {waitUntil: 'networkidle2'});
-    await page.waitForResponse(response =>
-    response.url().includes('/api/proposal/pdf/') &&
-    response.status() === 200
-    );
+    await page.waitForSelector('p.label-about', {timeout: 10000});
 
     const pdfBuffer = await page.pdf({
       path: 'roteiro.pdf',
